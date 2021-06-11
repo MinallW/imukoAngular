05c15d154aac4f2877422bb38a10c9bf52a56b59
MinallW <suarezmiguelc23@gmail.com>
2021-06-11 02:44:56 -0400

    simple form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSliderModule } from '@angular/material/slider';
+import { RegisterComponent } from './register/register.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+	AppComponent,
+	RegisterComponent
+    ],
+    imports: [
+	BrowserModule,
+	AppRoutingModule,
+	BrowserAnimationsModule,
+	MatSliderModule,
+	MatInputModule,
+	MatButtonModule,
+	MatSelectModule,
+	MatRadioModule,
+	MatCardModule,
+	ReactiveFormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
